89c0b6395f8528298d11aa19527055c940cf4142
JGSalgueiro <j.g.salgueiro@gmail.com>
2021-07-02 22:42:53 +0100

    model_builder init

---
+++ RBS_Classifier/model_builder.py
@@ -0,0 +1,28 @@
+**********************************************************************
+* RBS_Classifier                                                     *
+* tensorflow (version 2.0 or higher) and kerastuner                  *
+* Author : João Geraldes Salgueiro <joao.g.salgueiro@webmail.com>    *
+* All rights reserved to C2TN                                        *
+**********************************************************************
+import numpy as np 
+import tensorflow as tf 
+from kerastuner.tuners import RandomSearch
+from tensorflow import keras
+
+
+def build_model(hp):
+	#TODO
+	return 0
+
+def create_model():
+	#TODO
+	return 0
+
+def main():
+	model = create_model()
+
+
+if __name__ == "__main__":
+    main()
+
+
