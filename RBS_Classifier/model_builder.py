beaab84792a13189b59125d2508ba38245644ef4
JGSalgueiro <j.g.salgueiro@gmail.com>
2021-07-03 23:51:48 +0100

    Added initial given spectra

--- RBS_Classifier/model_builder.py
+++ RBS_Classifier/model_builder.py
@@ -1,13 +1,22 @@
-**********************************************************************
-* RBS_Classifier                                                     *
-* tensorflow (version 2.0 or higher) and kerastuner                  *
-* Author : João Geraldes Salgueiro <joao.g.salgueiro@webmail.com>    *
-* Developed with the help of C2TN                                    *
-**********************************************************************
+******************************************************************************
+* RBS_Classifier                                                             *
+* tensorflow (version 2.0 or higher) and kerastuner                          *
+* Author : João Geraldes Salgueiro <joao.g.salgueiro@tecnico.ulisboa.com>    *
+* Developed with the help of C2TN                                            *
+******************************************************************************
+
 import numpy as np 
 import tensorflow as tf 
 from kerastuner.tuners import RandomSearch
 from tensorflow import keras
+
+PEAK = 0.5
+
+def poisson_noise(data):
+	noise = np.random.poisson(image / 255.0 * PEAK) / PEAK * 255  #create noisy data_set
+	return noise
+
+def create_trainning_set():
 
 
 def build_model(hp):
