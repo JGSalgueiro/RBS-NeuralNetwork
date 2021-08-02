2f105d9661e3dabb19729ce38d7a8cb9e13d0609
JGSalgueiro <j.g.salgueiro@gmail.com>
2021-08-02 14:25:44 +0100

    V1.1

--- RBS_Classifier/create_set.py
+++ RBS_Classifier/create_set.py
@@ -18,7 +18,7 @@ import numpy as np
 DATADIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TrainingSet"
 TESTDIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TestSet"
 CATEGORIES = ["ag5au", "ag10au", "ag15au", "ag20au", "ag25au", "ag30au", "ag35au", "ag40au", "ag45au", "ag50au", "ag55au", "ag60au", "ag65au", "ag70au", "ag75au", "ag80au", "ag85au", "ag90au", "ag95au", "ag100au"]
-NUM_GENERATED = 100
+NUM_GENERATED = 3
 CHANNELS = 1024
 
 def insert_number(input):
@@ -27,14 +27,18 @@ def insert_number(input):
 
 
 def extract_number(input):
-	return float(input)
+	x = input.split("E")
+	if(x[1][0] == '+'): 
+		return float(x[0]) * (10 ** float(x[1][1:]))
+	elif(x[1][0] == '-'):
+		return float(x[0]) * (10 ** -float(x[1][1:]))
 
 
 def load_spectrum(path, categorie):
 	spectrum = []
 
 
-	path = os.path.join(DATADIR, categorie)
+	path = os.path.join(TESTDIR, categorie)
 	s = os.listdir(path)[0]
 
 	f = open(os.path.join(path,s))
@@ -47,7 +51,7 @@ def load_spectrum(path, categorie):
 		fit_num = extract_number(fit)
 		spectrum.append(fit_num)
 
-	return spectrum
+	return add_noise(spectrum)
 
 
 def add_noise(spectrum):
@@ -58,20 +62,24 @@ def generate_spectrum(spectrum_data, path):
 	fptr = open(path, "w")
 	index = 0
 	for num in spectrum_data:
+		print(num)
+		print(str(num))
 		line = str(index) + " x " + str(num) + "\n"
 		fptr.write(line)
+		index +=1
 
 	return 0
 
 
 def main():
 	for categorie in CATEGORIES:
-		spectrum_data = load_spectrum(DATADIR, categorie)
+		spectrum_data = load_spectrum(TESTDIR, categorie)
 		for i in range(NUM_GENERATED):
 			name = "gen" + str(i) + ".dat"
 			path = os.path.join(DATADIR, categorie)
 			file_path = os.path.join(path, name)
 
+			print(spectrum_data[1023])
 			generate_spectrum(spectrum_data, file_path)
 			
 
--- RBS_Classifier/model_builder.py
+++ RBS_Classifier/model_builder.py
@@ -16,9 +16,9 @@ import os
 DATADIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TrainingSet" #FIX DIRS
 TESTDIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TestSet"     #FIX DIRS
 CATEGORIES = ["ag5au", "ag10au", "ag15au", "ag20au", "ag25au", "ag30au", "ag35au", "ag40au", "ag45au", "ag50au", "ag55au", "ag60au", "ag65au", "ag70au", "ag75au", "ag80au", "ag85au", "ag90au", "ag95au", "ag100au"]
-CLASSIFICATION = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
+CLASSIFICATION = [0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0,90,0.95,1.00]
 PEAK = 0.5
-CHANNELS = 1024
+CHANNELS = 944
 MAX_DATA = 0
 MAX_FIT = 0
 
@@ -33,13 +33,24 @@ def sigmoide(x):
 def extract_number(input):
 	return float(input)
 
+def extract_number_v2(input):
+	x = input.split("E")
+	if(x[1][0] == 'x'): 
+		return float(x[0]) * (10 ** float(x[1]))
+	else:
+		return float(x[0]) * (10 ** -float(x[1]))
+
 
 def load_sets():
 	global train_spectra
 	global train_label
+	global test_spectra
+	global test_label
 	global MAX_DATA, MAX_FIT, MIN_FIT, MIN_DATA
 	labels = []
 	spectras = []
+	labels_test = []
+	spectras_test = []
 	#for each dir load training sets
 	for categorie in CATEGORIES:
 		path = os.path.join(DATADIR, categorie)
@@ -53,7 +64,6 @@ def load_sets():
 				fit = x[2]
 
 				fit_num = sigmoide(extract_number(fit))
-
 				spetra_data.append(fit_num)
 
 				if fit_num > MAX_FIT:
@@ -63,19 +73,45 @@ def load_sets():
 
 			#data.append(spetra_data)
 			spectras.append(spetra_data)
-			#labels.append(CLASSIFICATION[CATEGORIES.index(categorie)] / 100)
+			#labels.append(CLASSIFICATION[CATEGORIES.index(categorie)] * 100)
 			labels.append(CATEGORIES.index(categorie))
+
+	for categorie in CATEGORIES:
+		path = os.path.join(TESTDIR, categorie)
+		for spectra in os.listdir(path):
+			spetra_data = []
+			f = open(os.path.join(path,spectra))
+			for line in (f.readlines() [-CHANNELS:]):
+				x = line.split()
+				channel = x[0]
+				data = x[1]
+				fit = x[2]
+
+				fit_num = sigmoide(extract_number_v2(fit))
+				spetra_data.append(fit_num)
+
+				if fit_num > MAX_FIT:
+					MAX_FIT = fit_num
+				elif fit_num < MIN_FIT:
+					MIN_FIT = fit_num
+
+			#data.append(spetra_data)
+			spectras_test.append(spetra_data)
+			#labels_test.append(CLASSIFICATION[CATEGORIES.index(categorie)] * 100)
+			labels_test.append(CATEGORIES.index(categorie))
 
 
 	print("Loaded training inputs from : " + categorie + ";")
+
 	train_label = np.array(labels)
 	train_spectra = np.array(spectras)
+	test_label = np.array(labels_test)
+	test_spectra = np.array(spectras_test)
 	print("Finished loading sets;")
 
 
 def normalize(num):
 	global train_spectra
-
 	return num
 
 
@@ -87,74 +123,66 @@ def poisson_noise():
 def build_model_v1():
 	global train_spectra
 	global train_label
+	global test_spectra
+	global test_label
+
+
 	model = keras.Sequential([
 	keras.layers.Flatten(),
-	keras.layers.Dense(1024, input_dim = 1024, activation = 'relu'),
-	keras.layers.Dense(512, activation = 'relu'),
-	keras.layers.Dense(256, activation = 'relu'),
-	keras.layers.Dense(20, activation = 'softmax')
+    keras.layers.Dense(1024, input_dim = 1024, activation = 'relu'),
+    keras.layers.Dense(512, activation = 'relu'),
+    keras.layers.Dense(256, activation = 'relu'),
+    keras.layers.Dense(20, activation = 'softmax')
+    ])
+
+	"""
+	model = keras.Sequential([
+	keras.layers.Dense(1024, input_dim = 1024, activation = 'linear'),
+	keras.layers.Dense(1024, activation='tanh'),
+	keras.layers.Dense(512, activation='tanh'),
+	keras.layers.Dense(256, activation='tanh'),
+	keras.layers.Dense(1, activation='sigmoid')
 	])
+
+	sgd = keras.optimizers.RMSprop(lr=0.001)
+	model.compile(loss=keras.losses.mean_squared_error,optimizer=sgd, metrics=['mae'])"""
 
 	model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(), metrics = ['accuracy'])
 	#model.compile(optimizer='adam',loss='poisson', metrics=['accuracy'])
 
-	model.fit(train_spectra, train_label, epochs = 5, batch_size = 20)
+	model.fit(train_spectra, train_label, epochs = 50, batch_size = 128 , verbose=1, validation_data = (test_spectra, test_label))
 	return model
 
-def build_model(hp):
-	model = keras.Sequential()
-	model.add(keras.layers.Flatten()),
-	model.add(keras.layers.Dense(512, input_shape = (2,1024,1), activation = 'relu'))
-	model.add(keras.layers.Dense(256, activation = 'relu'),)
-
-	
-
-	"""for i in range(hp.Int("Conv layers", min_value = 1, max_value = 4)):
-		model.add(keras.layers.Conv1D(hp.Choice(f"layer_{i}_filters", [16,32,64]) ,3 ,activation = 'relu'))
-
-	model.add(keras.layers.MaxPool2D(2,2))
-	model.add(keras.layers.Dropout(0.5))
-	model.add(keras.layers.Flatten())
-
-	model.add(keras.layers.Dense(hp.Choice("Dense_layer", [64, 128, 256, 512, 1024]), activation = 'relu'))
-	model.add(keras.layers.Dense(3, activation = 'softmax'))"""
-
-	model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(), metrics = ['accuracy'])
-	return model
-
-
-def create_model():
-	global train_spectra
-	global train_label
-	#train_spectra = pad_sequences(train_spectra, maxlen = 20)
-	print(train_label)
-	tuner = RandomSearch(build_model, objective = 'val_accuracy', max_trials = 5)
-	tuner.search(train_spectra, train_label, validation_data = (train_spectra, train_label), epochs = 10, batch_size = 32)
-	return tuner.get_best_models()[0]
 
 
 def main(): 
 	global train_spectra
 	global train_label
-	i = 1
+	global test_spectra
+	global test_label
 	load_sets()
 	
 	model = build_model_v1()
 	model.save('./best_model')
 
 	
-	loaded_model = keras.models.load_model('./best_model')
+	#loaded_model = keras.models.load_model('./best_model')
 	print(train_spectra[1])
 	print(train_spectra.shape)
 
-	
-	for i in range(33):
-		load_spectra = np.expand_dims(train_spectra[i], axis=0)
-		results = loaded_model.predict(load_spectra)
+	"""
+	for i in range(10):
+		load_spectra = np.expand_dims(test_spectra[i], axis=0)
+		results = model.predict(load_spectra)
+
+		print(test_label[i])
+		#print(results)
 		
-		print("EXPECTED : " , CLASSIFICATION[train_label[i]] ,"%")
-		print("OBTAINED : " , CLASSIFICATION[np.argmax(results)], "%")
-	
+		print("EXPECTED : " , CATEGORIES[test_label[i]])
+		print("OBTAINED : " , CATEGORIES[np.argmax(results)])
+	"""
+
+	model.evaluate(test_spectra, test_label)
 	
 
 
