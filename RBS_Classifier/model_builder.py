d9bbecde2aa58fa742c672ad97bbb69abfce96da
João Geraldes Salgueiro <43580610+JGSalgueiro@users.noreply.github.com>
2021-08-24 17:32:20 +0100

    Update model_builder.py

--- RBS_Classifier/model_builder.py
+++ RBS_Classifier/model_builder.py
@@ -8,6 +8,9 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 import tensorflow as tf 
+import random
+import pickle
+from datetime import datetime
 from keras_tuner.tuners import RandomSearch
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow import keras
@@ -15,19 +18,35 @@ import os
 
 DATADIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TrainingSet" #FIX DIRS
 TESTDIR = "/home/jgsalgueiro/Desktop/RBS_ANN/RBS_NeuralNetwork/Training/TestSet"     #FIX DIRS
-CATEGORIES = ["ag5au", "ag10au", "ag15au", "ag20au", "ag25au", "ag30au", "ag35au", "ag40au", "ag45au", "ag50au", "ag55au", "ag60au", "ag65au", "ag70au", "ag75au", "ag80au", "ag85au", "ag90au", "ag95au", "ag100au"]
+CATEGORIES = ["ag5au", "ag10au", "ag15au", "ag20au", "ag25au", "ag30au", "ag35au", "ag40au", "ag45au", "ag50au", "ag55au", "ag60au", "ag65au", "ag70au", "ag75au", "ag80au", "ag85au", "ag90au", "ag95au", "ag100au", 
+              "cu5au", "cu10au", "cu15au", "cu20au", "cu25au", "cu30au", "cu35au", "cu40au", "cu45au", "cu50au", "cu55au", "cu60au", "cu65au", "cu70au", "cu75au", "cu80au", "cu85au", "cu90au", "cu95au", "cu100au"]
 CLASSIFICATION = [0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0,90,0.95,1.00]
-PEAK = 0.5
-CHANNELS = 944
-MAX_DATA = 0
-MAX_FIT = 0
 
-MIN_DATA = 9999999
-MIN_FIT = 999999
+CHANNELS = 1024
 
 
 def sigmoide(x):
 	return 1/(1 + np.exp(-x))
+
+
+def find_MAX(spectra_set):
+	MAX = 0
+
+	for spectrum in spectra_set:
+		if np.amax(spectrum) > MAX:
+			MAX = np.amax(spectrum)
+
+	
+	return MAX
+
+
+def normalize(spectra_set, MAX):
+	sets = []
+
+	for i in spectra_set:
+		sets.append(np.true_divide(i, MAX))
+
+	return np.array(sets).reshape(-1, 1024, 1)
 
 
 def extract_number(input):
@@ -35,47 +54,38 @@ def extract_number(input):
 
 def extract_number_v2(input):
 	x = input.split("E")
-	if(x[1][0] == 'x'): 
-		return float(x[0]) * (10 ** float(x[1]))
-	else:
-		return float(x[0]) * (10 ** -float(x[1]))
+	if(x[1][0] == '+'): 
+		return float(x[0]) * (10 ** float(x[1][1:]))
+	elif(x[1][0] == '-'):
+		return float(x[0]) * (10 ** -float(x[1][1:]))
 
 
 def load_sets():
-	global train_spectra
-	global train_label
-	global test_spectra
-	global test_label
-	global MAX_DATA, MAX_FIT, MIN_FIT, MIN_DATA
-	labels = []
-	spectras = []
-	labels_test = []
-	spectras_test = []
+	global train_spectras
+	global train_labels
+
+	training_dataset = []
+	test_dataset = []
+
 	#for each dir load training sets
 	for categorie in CATEGORIES:
 		path = os.path.join(DATADIR, categorie)
 		for spectra in os.listdir(path):
 			spetra_data = []
 			f = open(os.path.join(path,spectra))
-			for line in (f.readlines() [-CHANNELS:]):
+			for line in (f.readlines()[-CHANNELS:]):
 				x = line.split()
 				channel = x[0]
 				data = x[1]
 				fit = x[2]
 
-				fit_num = sigmoide(extract_number(fit))
+				fit_num = extract_number(fit)
 				spetra_data.append(fit_num)
 
-				if fit_num > MAX_FIT:
-					MAX_FIT = fit_num
-				elif fit_num < MIN_FIT:
-					MIN_FIT = fit_num
+			training_dataset.append([np.array(spetra_data), CATEGORIES.index(categorie)])
 
-			#data.append(spetra_data)
-			spectras.append(spetra_data)
-			#labels.append(CLASSIFICATION[CATEGORIES.index(categorie)] * 100)
-			labels.append(CATEGORIES.index(categorie))
 
+	#for each dir load testing sets
 	for categorie in CATEGORIES:
 		path = os.path.join(TESTDIR, categorie)
 		for spectra in os.listdir(path):
@@ -87,32 +97,45 @@ def load_sets():
 				data = x[1]
 				fit = x[2]
 
-				fit_num = sigmoide(extract_number_v2(fit))
+				fit_num = extract_number_v2(fit)
 				spetra_data.append(fit_num)
 
-				if fit_num > MAX_FIT:
-					MAX_FIT = fit_num
-				elif fit_num < MIN_FIT:
-					MIN_FIT = fit_num
-
-			#data.append(spetra_data)
-			spectras_test.append(spetra_data)
-			#labels_test.append(CLASSIFICATION[CATEGORIES.index(categorie)] * 100)
-			labels_test.append(CATEGORIES.index(categorie))
+			test_dataset.append([np.array(spetra_data), CATEGORIES.index(categorie)])
 
 
-	print("Loaded training inputs from : " + categorie + ";")
+	random.shuffle(training_dataset)
+	train_spectras = []
+	train_labels = []
+	test_spectras = []
+	test_labels = []
 
-	train_label = np.array(labels)
-	train_spectra = np.array(spectras)
-	test_label = np.array(labels_test)
-	test_spectra = np.array(spectras_test)
-	print("Finished loading sets;")
+	for spectra, label in training_dataset:
+		train_spectras.append(spectra)
+		train_labels.append(label)
 
+	for spectra, label in test_dataset:
+		test_spectras.append(spectra)
+		test_labels.append(label)
 
-def normalize(num):
-	global train_spectra
-	return num
+	train_spectras = np.array(train_spectras).reshape(-1, 1024, 1)
+	test_spectras = np.array(test_spectras).reshape(-1, 1024, 1)
+
+	pickle_out = open("Train_Spectra.pickle", "wb")
+	pickle.dump(train_spectras, pickle_out)
+	pickle_out.close()
+
+	pickle_out = open("Train_Label.pickle", "wb")
+	pickle.dump(train_labels, pickle_out)
+	pickle_out.close()
+
+	pickle_out = open("Test_Spectra.pickle", "wb")
+	pickle.dump(test_spectras, pickle_out)
+	pickle_out.close()
+
+	pickle_out = open("Test_Label.pickle", "wb")
+	pickle.dump(test_labels, pickle_out)
+	pickle_out.close()
+
 
 
 def poisson_noise():
@@ -120,37 +143,25 @@ def poisson_noise():
 	print(s)
 	return 0
 
-def build_model_v1():
+def build_model():
 	global train_spectra
 	global train_label
 	global test_spectra
 	global test_label
 
-
 	model = keras.Sequential([
+	keras.layers.Conv1D(512, 3,activation = 'relu', input_shape = (1024,1)),
+	keras.layers.MaxPooling1D(2),
+	keras.layers.Conv1D(256, 3,activation = 'relu', input_shape = (1024,1)),
+	keras.layers.MaxPooling1D(2),
 	keras.layers.Flatten(),
-    keras.layers.Dense(1024, input_dim = 1024, activation = 'relu'),
-    keras.layers.Dense(512, activation = 'relu'),
-    keras.layers.Dense(256, activation = 'relu'),
-    keras.layers.Dense(20, activation = 'softmax')
-    ])
-
-	"""
-	model = keras.Sequential([
-	keras.layers.Dense(1024, input_dim = 1024, activation = 'linear'),
-	keras.layers.Dense(1024, activation='tanh'),
-	keras.layers.Dense(512, activation='tanh'),
-	keras.layers.Dense(256, activation='tanh'),
-	keras.layers.Dense(1, activation='sigmoid')
+    keras.layers.Dense(64),
+    keras.layers.Dense(40, activation = 'softmax'),
 	])
 
-	sgd = keras.optimizers.RMSprop(lr=0.001)
-	model.compile(loss=keras.losses.mean_squared_error,optimizer=sgd, metrics=['mae'])"""
-
 	model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(), metrics = ['accuracy'])
-	#model.compile(optimizer='adam',loss='poisson', metrics=['accuracy'])
 
-	model.fit(train_spectra, train_label, epochs = 50, batch_size = 128 , verbose=1, validation_data = (test_spectra, test_label))
+	model.fit(train_spectra, train_label, epochs = 10, batch_size = 128, validation_split = 0.05)
 	return model
 
 
@@ -160,30 +171,46 @@ def main():
 	global train_label
 	global test_spectra
 	global test_label
-	load_sets()
+
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Loading data ...")
+	#load_sets()
 	
-	model = build_model_v1()
+	pickle_in = open("Train_Spectra.pickle", "rb")
+	train_spectra = np.array(pickle.load(pickle_in))
+	pickle_in = open("Train_Label.pickle", "rb")
+	train_label = np.array(pickle.load(pickle_in))
+	pickle_in = open("Test_Spectra.pickle", "rb")
+	test_spectra = np.array(pickle.load(pickle_in))
+	pickle_in = open("Test_Label.pickle", "rb")
+	test_label = np.array(pickle.load(pickle_in))
+
+
+
+	MAX_TRAIN = find_MAX(train_spectra)
+	MAX_TEST = find_MAX(test_spectra)
+
+	train_spectra = normalize(train_spectra, MAX_TRAIN)
+	test_spectras = normalize(test_spectra, MAX_TEST)
+
+	"""
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Loaded training spectras with shape : " ,train_spectra.shape)
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Loaded testing spectras with shape  : " ,test_spectra.shape)
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Will now start training model. ")
+	model = build_model()
 	model.save('./best_model')
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Saved model : SUCESS")
+	print(datetime.now().strftime("%H:%M:%S") , " - " , "Model Training - SUCESS : will now avaluate the performance. ")
 
-	
-	#loaded_model = keras.models.load_model('./best_model')
-	print(train_spectra[1])
-	print(train_spectra.shape)
+
+
 
 	"""
-	for i in range(10):
-		load_spectra = np.expand_dims(test_spectra[i], axis=0)
-		results = model.predict(load_spectra)
 
-		print(test_label[i])
-		#print(results)
-		
-		print("EXPECTED : " , CATEGORIES[test_label[i]])
-		print("OBTAINED : " , CATEGORIES[np.argmax(results)])
-	"""
 
-	model.evaluate(test_spectra, test_label)
-	
+	loaded_model = keras.models.load_model('./best_model')
+	print(loaded_model.evaluate(train_spectra, train_label))
+
+
 
 
 if __name__ == "__main__":
